3756665eadfa1e733ef97e31e81a1af533494219
julioortiz16 <julioortizbort@gmail.com>
2026-05-06 14:08:09 +0200

    solo falta los ultimos

---
+++ part1/Ejemplos/1/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+    return (
+      <button onClick={onClick}>
+        {text}
+      </button>
+    )
+  }
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+
+const handleLeftClick = () => {
+  setAll(allClicks.concat('L'))
+  setLeft(left + 1)
+  setTotal(total + 1)
+}
+
+const handleRightClick = () => {
+  setAll(allClicks.concat('R'))
+  setRight(right + 1)
+  setTotal(total + 1)
+}
+
+
+ /* const Display = ({ counter }) => {
+    return (
+      <div>{counter}</div>
+    )
+  }
+ */
+
+  
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      {right}
+      <Button onClick={handleRightClick} text="right" />
+      <History allClicks={allClicks} />
+      <p>Total clicks: {total}</p>
+    </div>
+  )
+}
+
+export default App
